2fc95b5595f951c2d8756e4b01d123d705f6c12a
Adi Aryasuta <adiaryasuta.dev@gmail.com>
2023-02-06 08:02:04 +0800

    Add middleware

--- app/controller/AuthController.php
+++ app/controller/AuthController.php
@@ -8,12 +8,22 @@ class AuthController extends Controller
 {
 	public function login()
 	{
+		// Middleware
+		if (auth()->user()) {
+			return redirect('/');
+		}
+
 		return view('login')
 			->useLayout(new AuthLayout(), ['title' => 'Masuk']);
 	}
 
 	public function authenticate()
 	{
+		// Middleware
+		if (auth()->user()) {
+			return redirect('/');
+		}
+
 		$credentials = $this->request->only(['username', 'password']);
 
 		if (auth()->attempt($credentials)) {
--- app/controller/HomeController.php
+++ app/controller/HomeController.php
@@ -8,6 +8,11 @@ class HomeController extends Controller
 {
 	public function index()
 	{
+		// Middleware
+		if (auth()->guest()) {
+			return redirect('/login');
+		}
+
 		return view('dashboard')
 			->useLayout(new MainLayout(), ['title' => 'Dashboard']);
 	}
